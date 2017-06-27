c935e4db5fbdffdf45eb8f4ccc8ba56a8052f609
lburgazzoli <lburgazzoli@gmail.com>
2017-06-27 07:58:16 +0200

    Add leadership listener example

---
+++ atomix-boot-examples/atomix-client-leadership/src/main/java/com/github/lburgazzoli/atomix/boot/examples/leadership/ClientLeadershipListener.java
@@ -0,0 +1,46 @@
+package com.github.lburgazzoli.atomix.boot.examples.leadership;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import io.atomix.AtomixClient;
+import io.atomix.group.DistributedGroup;
+import io.atomix.group.LocalMember;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClientLeadershipListener {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClientLeadershipListener.class);
+
+    @Autowired
+    AtomixClient client;
+    @Value("${pod.namespace}")
+    String groupName;
+    @Value("${pod.name}")
+    String memberName;
+
+    private LocalMember member;
+
+    @PostConstruct
+    public void initialize() {
+        LOGGER.info("Getting group '{}'", groupName);
+        DistributedGroup group = client.getGroup(groupName).join();
+
+        LOGGER.info("Listen election event");
+        group.election().onElection(t -> LOGGER.info("Member election '{}", t.leader()));
+
+        LOGGER.info("Joining group '{}' as '{}'", groupName, memberName);
+        member = group.join(memberName).join();
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        if (member != null) {
+            member.leave().join();
+        }
+    }
+}
